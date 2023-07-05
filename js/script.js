190312a98d09c563e6ebb435bae0c9894071391b
freedommk <mari.kuprava@gmail.com>
2023-07-05 03:42:03 -0700

    error fixed in html

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
 button.addEventListener("click", function () {
-  modal.classList.add("show-modal");
-  button.innerText = "You’ve got this!!!";
+    modal.classList.add("show-modal");
+    button.innerText = "You've got this!!!";
 });
 modalX.addEventListener("click", function () {
-  modal.classList.remove("show-modal");
-  button.innerText = "Who’s got this?";
+    modal.classList.remove("show-modal");
+    button.innerText = "Who's got this?";
 });
